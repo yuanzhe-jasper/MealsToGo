f00c73184608b9a1e99fe7913eaeb99ccc1b6ae6
Yuanzhe-Li <li.yuanzhe@northeastern.edu>
2022-05-17 16:14:53 -0700

    Mobile Application

---
+++ MealsToGoApp/App.js
@@ -0,0 +1,53 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
+
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAo-EiUNXyABBHB2g99tLC7Xr3et-tI5r0",
+  authDomain: "mealstogo-ada80.firebaseapp.com",
+  projectId: "mealstogo-ada80",
+  storageBucket: "mealstogo-ada80.appspot.com",
+  messagingSenderId: "442200584457",
+  appId: "1:442200584457:web:e1489780edfa98d13f23d8"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
---
+++ MealsToGoApp/src/components/favourites/favourite.component.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import styled from "styled-components/native";
+import { AntDesign } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+
+import { FavouritesContext } from "../../services/favourites/favourites.context";
+
+const FavouriteButton = styled(TouchableOpacity)`
+  position: absolute;
+  top: 25px;
+  right: 25px;
+  z-index: 9;
+`;
+
+export const Favourite = ({ restaurant }) => {
+  const { favourites, addToFavourites, removeFromFavourites } = useContext(
+    FavouritesContext
+  );
+
+  const isFavorite = favourites.find((r) => r.placeId === restaurant.placeId);
+
+  return (
+    <FavouriteButton
+      onPress={() => !isFavorite? addToFavourites(restaurant) : removeFromFavourites(restaurant)}
+    >
+      <AntDesign
+        name={isFavorite ? "heart" : "hearto"}
+        size={24}
+        color={isFavorite?"red":"white"}
+      />
+    </FavouriteButton>
+  );
+};
